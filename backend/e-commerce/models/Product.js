4734dfbfc3fc8990ff8be9e2d3226b280ad666e6
Braian Aguada <braianaguada@gmail.com>
2022-11-15 01:23:02 -0300

    Update Product.js

--- backend/e-commerce/models/Product.js
+++ backend/e-commerce/models/Product.js
@@ -1,11 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const ProductSchema = new mongoose.Schema({
-    name: { type: String, required: true},
-    color: {type: String, required: true},
-    category: {type: String, required: true},
-    price: {type: String, required: true},
-    description: {type: String, required: true},
-}, {timestamps: true});
+const ProductSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    color: { type: String, required: true },
+    category: { type: String, required: true },
+    price: { type: String, required: true },
+    description: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("Product", ProductSchema, 'products');
+module.exports = mongoose.model("Product", ProductSchema, "products");
