597c1a0f9f2ea47ae487ebbe52bc4c3d65711a47
candrepa1 <candrepa1@gmail.com>
2022-11-14 20:23:30 -0500

    add first collection model and create a seed script

---
+++ backend/e-commerce/models/Product.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+    name: { type: String, required: true},
+    color: {type: String, required: true},
+    category: {type: String, required: true},
+    price: {type: String, required: true},
+    description: {type: String, required: true},
+}, {timestamps: true});
+
+module.exports = mongoose.model("Product", ProductSchema, 'products');
---
+++ backend/e-commerce/runProductSeeds.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const { faker } = require('@faker-js/faker');
+const mongoose = require("mongoose");
+
+const dbConnect = require("./config/mongo");
+const Product = require('./models/Product')
+
+const seedDB = async () => {
+    try {
+        const client = dbConnect();
+
+        for (let i = 0; i < 20; i++) {
+            const name = faker.commerce.productName();
+            const color = faker.commerce.color();
+            const category = faker.commerce.department();
+            const price = faker.commerce.price();
+            const description = faker.commerce.productDescription();
+
+            const newProduct = new Product({name, color, category, price, description});
+            await newProduct.save();
+        }
+
+        console.log("Database seeded! :)");
+        mongoose.connection.close()
+    } catch (err) {
+        console.log(err.stack);
+    }
+}
+
+seedDB();
