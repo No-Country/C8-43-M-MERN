ebcf73e395357b707038e90387ab67fcb3192a74
Braian Aguada <braianaguada@gmail.com>
2022-11-15 18:28:55 -0300

    Update products.js

--- backend/e-commerce/routes/products.js
+++ backend/e-commerce/routes/products.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const { getProducts, getProduct, getProductSearch } = require("../controllers/products");
 const { validatorGetItem } = require("../validators/products");
 
-router.get("/", getProductSearch)
 router.get("/", getProducts);
 router.get("/:id", validatorGetItem, getProduct);
+router.get("/", getProductSearch)
 
 module.exports = router;
