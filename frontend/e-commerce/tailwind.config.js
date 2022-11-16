52f2b66443e77ed97209670bff7d2d5317a4660f
Ariel Martinez <arielstereo@msn.com>
2022-11-16 12:09:25 -0300

    Home page with navbar

--- frontend/e-commerce/tailwind.config.js
+++ frontend/e-commerce/tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.html",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("flowbite/plugin")
+  ],
 }
