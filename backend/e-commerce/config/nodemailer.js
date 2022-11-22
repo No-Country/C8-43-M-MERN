b35c349c846fb22b212d5e038349382019c0496f
Braian Aguada <braianaguada@gmail.com>
2022-11-22 02:53:39 -0300

    Update nodemailer.js

--- backend/e-commerce/config/nodemailer.js
+++ backend/e-commerce/config/nodemailer.js
@@ -28,12 +28,7 @@ const sendEmail = async (email, subject, html) => {
 
 const getTemplate = (name, token) => {
   return `
-      <head>
-          <link rel="stylesheet" href="./style.css">
-      </head>
-      
       <div id="email___content">
-          <img src="https://i.imgur.com/eboNR82.png" alt="">
           <h2>Hola ${name}</h2>
           <p>Para confirmar tu cuenta, ingresa al siguiente enlace</p>
           <a
