c9a5717a73e7957a037285363f53ee81793a71c8
Ariel Martinez <arielstereo@msn.com>
2022-12-06 13:10:23 -0300

    Add context

--- frontend/e-commerce/components/cart/ArticleCart.jsx
+++ frontend/e-commerce/components/cart/ArticleCart.jsx
@@ -1,41 +1,56 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import { useProduct } from "../../context/ProductContext";
 
 
-const ArticleCart = ({setIsActiveModal}) => {
-    return (
-        <article className='flex flex-col justify-center items-center gap-[20px] mb-[60px]'>
+const ArticleCart = ({ setIsActiveModal }) => {
+  const { cart } = useProduct();
 
-            <header className='w-full'>
-                <h3 className='font-[500] text-[32px] text-[#1B5B45]'>Camisa florel</h3>
-            </header>
+  return (
+    <article className="flex flex-col justify-center items-center gap-[20px] mb-[60px]">
+        {cart.map((cart) =>(
+      <div>
+        <header className="w-full">
+          <h3 className="font-[500] text-[32px] text-[#1B5B45]">
+            {cart.name}
+          </h3>
+        </header>
 
-            <div className='flex flex-col gap-[20px]'>
+        <div className="flex flex-col gap-[20px]">
+          <Image src={cart.image.url} width="800" height="800" alt={cart.name} className="w-[380px] h-[290px] rounded-2xl"/>
 
-                <header className="w-[380px] h-[290px] bg-black"></header>
+          <div className="bg-[#F4F0BB] w-[380px] h-[135px] rounded-[25px] flex flex-col justify-center">
+            <ul className="m-[25px]">
+              <li className="flex justify-between ">
+                <p className="font-[500] text-[20px] text-[#1B5B45]">
+                  Talle: L
+                </p>
+                <p className="font-[500] text-[20px] text-[#1B5B45]">
+                  Precio: $ {cart.price}
+                </p>
+              </li>
+              <li className="flex justify-between">
+                <p className="font-[500] text-[20px] text-[#1B5B45]">
+                  Color: {cart.color}
+                </p>
+              </li>
+              <li className="flex justify-between">
+                <p className="font-[500] text-[20px] text-[#1B5B45]">
+                  Cantidad: 1
+                </p>
+                <button
+                  onClick={() => setIsActiveModal(true)}
+                  className="w-[155px] h-[35px] rounded-[8px] border-[#1B5B45] border-[2px] flex items-center justify-center hover:bg-[#1B5B45] text-[#1B5B45] hover:text-[#F4F0BB] duration-[0.2s]"
+                >
+                  <p className="font-[500] text-[20px] ">Deseleccionar</p>
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>))}
+    </article>
+  );
+};
 
-                <div className='bg-[#F4F0BB] w-[380px] h-[135px] rounded-[25px] flex flex-col justify-center'>
-                    <ul className='m-[25px]'>
-                        <li className='flex justify-between '>
-                            <p className='font-[500] text-[20px] text-[#1B5B45]'>Talle: L</p>
-                            <p className='font-[500] text-[20px] text-[#1B5B45]'>Precio: 17USD</p>
-                        </li>
-                        <li className='flex justify-between'>
-                            <p className='font-[500] text-[20px] text-[#1B5B45]'>Color: Verde suave</p>
-                        </li>
-                        <li className='flex justify-between'>
-                            <p className='font-[500] text-[20px] text-[#1B5B45]'>Cantidad: 1</p>
-                            <button onClick={() => setIsActiveModal(true)} className='w-[155px] h-[35px] rounded-[8px] border-[#1B5B45] border-[2px] flex items-center justify-center hover:bg-[#1B5B45] text-[#1B5B45] hover:text-[#F4F0BB] duration-[0.2s]'>
-                                <p className='font-[500] text-[20px] '>Deseleccionar</p>
-                            </button>
-                        </li>
-                    </ul>
-                </div>
-
-            </div>
-
-
-        </article>
-    )
-}
-
-export default ArticleCart
+export default ArticleCart;
--- frontend/e-commerce/pages/carrito.jsx
+++ frontend/e-commerce/pages/carrito.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
+import { useProduct } from "../context/ProductContext";
 import ArticleCart from "../components/cart/ArticleCart";
 import DeleteModalCart from "../components/cart/DeleteModalCart";
 import Layout from "../components/Layout";
 import Link from "next/link";
 
 
+
 function Carrito() {
 
-    const [isActiveModal, setIsActiveModal] = useState(false)
+    const [isActiveModal, setIsActiveModal] = useState(false);
+    const { cart } = useProduct();
 
     return (
         <Layout>
@@ -69,27 +72,27 @@ function Carrito() {
                                     </div>
 
                                 </header>
-
+                                {cart.map((cart) => (
                                 <div className='w-[380px] h-[240px] rounded-[25px] bg-[#F4D58D] flex items-center justify-center'>
                                     <ul className='w-full m-[25px] mb-[10px] font-medium '>
                                         <li className='flex justify-between'>
                                             <p className="font-[500] text-[20px]">Precio:</p>
-                                            <p className="font-[500] text-[20px]">17 USD</p>
+                                            <p className="font-[500] text-[20px]">{cart.price}</p>
                                         </li>
-                                        <li className='flex justify-between'>
+                                        {/* <li className='flex justify-between'>
                                             <p className="font-[500] text-[20px]">IVA 33%:</p>
                                             <p className="font-[500] text-[20px]">23.66 USD</p>
-                                        </li>
+                                        </li> */}
                                         <li className='flex justify-between'>
                                             <p className="font-[500] text-[20px]">Envio:</p>
                                             <p className="font-[500] text-[20px]"> 500 ARS</p>
                                         </li>
                                         <li className='flex justify-between mt-[60px]'>
                                             <p className="font-[500] text-[20px]">Total a pagar</p>
-                                            <p className='font-[700] text-[28px]'>7598 ARS</p>
+                                            <p className='font-[700] text-[28px]'>$ {cart.price + 500}</p>
                                         </li>
                                     </ul>
-                                </div>
+                                </div>))}
 
                                 <Link href='/compra-completa'>
                                     <button className='w-[375px] h-[50px] rounded-[15px] bg-[#1B5B45] font-medium text-white mt-[60px]'>
