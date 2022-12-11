5568bcd186ec8ddb3c3b234e6283e52b5856d7f9
Gonzalo-Barra <gonzalobarravalle@gmail.com>
2022-12-11 13:59:14 -0300

    added formik and yup

--- frontend/e-commerce/pages/auth/signin.js
+++ frontend/e-commerce/pages/auth/signin.js
@@ -1,90 +1,88 @@
-import React from "react";
+
+
 import Link from "next/link";
 import { useRouter } from "next/router";
-import Image from "next/image";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import axios from "axios";
+import * as Yup from "yup";
 
-function signin() {
+export default function Signin() {
   const router = useRouter();
-  // Handles the submit event on form submit.
-  const handleSubmit = async (event) => {
-    // Stop the form from submitting and refreshing the page.
-    event.preventDefault();
-
-    // Get data from the form.
-    const data = {
-      email: event.target.email.value,
-      password: event.target.password.value,
-    };
-
-    // Send the data to the server in JSON format.
-    const JSONdata = JSON.stringify(data);
-
-    // API endpoint where we send form data.
-    const endpoint = "http://localhost:4000/auth/login";
-
-    // Form the request for sending data to the server.
-    const options = {
-      // The method is POST because we are sending data.
-      method: "POST",
-      // Tell the server we're sending JSON.
-      headers: {
-        "Content-Type": "application/json",
-      },
-      // Body of the request is the JSON data we created above.
-      body: JSONdata,
-    };
-
-    // Send the form data to our forms API on Vercel and get a response.
-    const response = await fetch(endpoint, options);
-    if (response.ok) {
-      return router.push("/");
-    }
-    // Get the response data from server as JSON.
-    // If server returns the name submitted, that means the form works.
-   
-  };
 
   return (
-    <div className="grid grid-cols-[1fr_1fr_1fr_1fr] h-screen place-items-center bg-[#1B5B45]">
-      <div className="pt-10 mb-0 col-start-2 col-end-4  mx-1">
-       
-        <Image src="/img/logo2.png" width={500} height={500} />
-        <Image src="/img/subtitle.png" width={200} height={200} />
-      </div>
-      <form onSubmit={handleSubmit} className=" rounded px-8 pt-6 pb-8 mb-4 col-start-2 col-end-4 row-start-2">
-       <div class=""> 
-       <label htmlFor="email" className="text-[#F4F0BB]">E-mail</label>
-        <br />
-        <input
-          className="shadow-2xl bg-[#F4F0BB]  rounded-md w-full py-2"
-          type="email"
-          id="email"
-          name="email"
-          required
-        />
-       </div>
-        <br />
-        <labe htmlFor="password" className="text-[#F4F0BB]">Contraseña</labe>
-        <br />
-        <input
-           className="shadow-2xl bg-[#F4F0BB]  rounded-md w-full py-2"
-          type="password"
-          id="password"
-          name="password"
-          required
-        />
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      validationSchema={Yup.object({
+        email: Yup.string().email("Correo no válido").required("Requerido"),
+        password: Yup.string().required("Requerido"),
+      })}
+     	
+		 onSubmit={async(values) => {
+   
+      axios({
+          method: "POST",
+          url: "https://c8-43-m-mern-api.vercel.app/auth/login",
+          data: { email: values.email, password: values.password },
+        }).then((res) => {
+          console.log(res);
+          if (res.status == 200) {
+            return router.push("/");
+          }
+          else if(res.error) {
+            alert("Usuario o contraseña incorrectos")
+          }
+        })
+        .catch((res) => {
+          alert("Usuario o contraseña incorrectos")
+        });
+      
+      }}
+    >
+      <div className="grid grid-cols-[1fr_1fr_1fr_1fr] h-screen place-items-center bg-[#1B5B45]">
+        <Form className=" rounded px-8 pt-6 pb-8 mb-4 col-start-2 col-end-4 row-start-2">
+          <div>
+            <label htmlFor="email" className="text-[#F4F0BB]">
+              E-mail
+            </label>
+            <br />
+            <Field
+              className="shadow-2xl bg-[#F4F0BB] text-black rounded-md w-full py-2"
+              name="email"
+              type="email"
+            />
+            <ErrorMessage name="email" />
+          </div>
 
-        <p  className="text-white text-center py-4">¿Olvidaste la contraseña?</p>
-        <button    className="shadow-2xl bg-[#F4F0BB]  rounded-md w-full py-2 my-4" type="submit">
-          Iniciar sesión
-        </button>
-        <Link  href="/auth/signup">
-          <button    className="shadow-2xl bg-[#F4F0BB]  rounded-md w-full py-2">Crear una cuenta</button>
-        </Link>
-       
-      </form>
-    </div>
+          <br />
+          <div>
+            <label htmlFor="password" className="text-[#F4F0BB]">
+              Contraseña
+            </label>
+            <br />
+            <Field
+              className="shadow-2xl bg-[#F4F0BB] text-black rounded-md w-full py-2"
+              name="password"
+              type="password"
+            />
+            <ErrorMessage name="password" />
+          </div>
+
+          <p className="text-white text-center py-4">
+            ¿Olvidaste la contraseña?
+          </p>
+          <button
+            className="shadow-2xl bg-[#F4F0BB]  rounded-md w-full py-2 my-4"
+            type="submit"
+          >
+            Iniciar sesión
+          </button>
+          <Link href="/auth/signup">
+            <button className="shadow-2xl bg-[#F4F0BB]  rounded-md w-full py-2">
+              Crear una cuenta
+            </button>
+          </Link>
+        </Form>
+      </div>
+    </Formik>
   );
 }
-
-export default signin;
--- frontend/e-commerce/pages/auth/signup.js
+++ frontend/e-commerce/pages/auth/signup.js
@@ -1,117 +1,124 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
-import axios from 'axios'
+import axios from "axios";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
-
-function signup() {
+export default function Signup() {
   const router = useRouter();
-  const [inputs, setInputs] = useState({});
-
-  const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setInputs((values) => ({ ...values, [name]: value }));
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    const data = {
-      name: inputs.name,
-      lastname: inputs.lastname,
-      email: inputs.email,
-      password: inputs.password,
-      role: inputs.role,
-      sex: inputs.sex,
-    };
-
-    
-    const endpoint = "http://localhost:4000/auth";
-
-    const result = await axios.post(endpoint, data);
-    if (result.status == 200) {
-      return router.push("/");
-    }
-  };
-
   return (
+    <Formik
+      initialValues={{
+        name: "",
+        lastname: "",
+        email: "",
+        password: "",
+        role: "",
+        sex: "",
+        termsUser: false,
+      }}
+      validationSchema={Yup.object({
+        name: Yup.string(),
+        lastname: Yup.string().required("Requerido"),
+        email: Yup.string().email("Correo no válido").required("Requerido"),
+        password: Yup.string().required("Requerido"),
+        role: Yup.array().oneOf(["user","seller"],  "Solo puedes elegir una opción").required("Requerido"),
+        sex: Yup.array().oneOf(["male","female"], "Solo puedes elegir una opción").required("Requerido"),
+        termsUser: Yup.boolean().oneOf([true], "Debe aceptar los términos"),
+      })}
+      onSubmit={async (values) => {
+        axios({
+          method: "POST",
+          url: "https://c8-43-m-mern-api.vercel.app/auth",
+          data: {
+            name: values.name,
+            lastname: values.lastname,
+            email: values.email,
+            password: values.password,
+            role: values.role,
+            sex: values.sex,
+            termsUser: values.termsUser,
+          },
+        })
+          .then((res) => {
+            console.log(res);
+            if (res.status == 200) {
+              alert("Usuario Creado");
+              return router.push("/");
+            } else if (res.error) {
+              alert("Usuario ya registrado");
+            }
+          })
+          .catch((res) => {
+            alert("Usuario ya registrado");
+          });
+      }}
+    >
+      <div className="bg-[#1B5B45] grid grid-cols-[1fr_1fr_1fr_1fr] grid-rows-[0.2fr_1fr_0.2fr_1fr] gap-4 h-full place-items-center">
+        <img
+          className=" col-start-2 col-end-4  pt-16 mx-1"
+          src="/img/logo2.png"
+          width="300"
+          height="300"
+          alt="logo"
+        />
+        <Form className="gap-0 col-start-2 col-end-4 row-start-2">
+          <div className="formulario__nombre flex justify-between items-center m-0">
+            <div className="flex flex-col m-0 py-4 ">
+              <label htmlFor="name" className="text-white text-sm font-medium">
+                Nombre
+              </label>
+              <Field
+                className="shadow-2xl  bg-[#F4F0BB]  text-black rounded-md py-1"
+                type="text"
+                name="name"
+                required
+              />
+  
+            </div>
+            
+            <div className="flex flex-col col-start-3 col-end-4">
+              <label
+                className="text-white  text-sm font-medium"
+                htmlFor="lastname"
+              >
+                Apellido
+              </label>
+              <Field
+                className="shadow-2xl  bg-[#F4F0BB] text-black  rounded-md py-1"
+                type="text"
+                name="lastname"
+                required
+              />
+            </div>
+          
+          </div>
 
-    <div className="bg-[#1B5B45] grid grid-cols-[1fr_1fr_1fr_1fr] grid-rows-[0.2fr_1fr_0.2fr_1fr]  gap-4 h-screen place-items-center">
-      <img
-        className=" col-start-2 col-end-4  pt-10 mx-1"
-        src="/img/logo2.png"
-        width="300"
-        height="300"
-        alt="logo"
-      />
-      <form
-        className="gap-0 col-start-2 col-end-4 row-start-2 "
-        onSubmit={handleSubmit}
-      >
-        <div className="formulario__nombre flex justify-between items-center m-0">
-          <div className="flex flex-col m-0 py-4 ">
-            <label className="text-white text-sm font-medium" htmlFor="name">
-              Nombre
+          <div className="formulario__email col-start-2 col-end-4  py-1">
+            <label className="text-white" htmlFor="email">
+              Email
             </label>
-            <input
-              className="shadow-2xl  bg-[#F4F0BB]  rounded-md py-1"
-              type="text"
-              id="name"
-              name="name"
+            <Field
+              className="shadow-2xl bg-[#F4F0BB]  text-black  rounded-md w-full py-1"
+              type="email"
+              name="email"
               required
-              value={inputs.name || ""}
-              onChange={handleChange}
             />
+      
           </div>
-          <div className="flex flex-col col-start-3 col-end-4">
-            <label
-              className="text-white  text-sm font-medium"
-              htmlFor="lastname"
-            >
-              Apellido
-            </label>
-            <input
-              className="shadow-2xl  bg-[#F4F0BB]  rounded-md py-1"
-              type="text"
-              id="lastname"
-              name="lastname"
-              required
-              value={inputs.lastname || ""}
-              onChange={handleChange}
-            />
-          </div>
-        </div>
-
-        <div className="formulario__email col-start-2 col-end-4  py-1">
-          <label className="text-white" htmlFor="email">
-            Email
-          </label>
-          <input
-            className="shadow-2xl bg-[#F4F0BB]  rounded-md w-full py-1"
-            type="email"
-            id="email"
-            name="email"
-            required
-            value={inputs.email || ""}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="formulario__contrasena flex justify-between items-center py-6 ">
-          <div className="flex flex-col m-0">
-            <label htmlFor="password" className="text-white">
-              Contraseña
-            </label>
-            <input
-              className=" shadow-2xl bg-[#F4F0BB]  rounded-md py-1"
-              type="password"
-              id="password"
-              name="password"
-              required
-              value={inputs.password || ""}
-              onChange={handleChange}
-            />
-          </div>
-          {/*           
+          <div className="formulario__contrasena flex justify-between items-center py-6 ">
+            <div className="flex flex-col m-0">
+              <label htmlFor="password" className="text-white">
+                Contraseña
+              </label>
+              <Field
+                className=" shadow-2xl bg-[#F4F0BB] text-black  rounded-md py-1"
+                type="password"
+                name="password"
+              />
+              <ErrorMessage name="password" />
+            </div>
+            {/*           
           <div className="m-0">
             <label htmlFor="password" className="text-white flex flex-col">
               Confirmar contraseña
@@ -126,100 +133,90 @@ function signup() {
               onChange={handleChange}
             />
           </div> */}
-        </div>
+          </div>
 
-        <div className="flex flex-col">
-          <h3 className="text-center text-white">¿Qué te gustaría hacer</h3>
-          <br />
-          <div className="py-5 px-4 flex">
-            <div>
-              <label htmlfor="role" className="text-white">
-                Promover y diseñar productos
-              </label>
-              <input
-                className="px-4"
-                type="checkbox"
-                id="seller"
-                name="role"
-                value="seller"
-                onChange={handleChange}
-              />
-            </div>
-            <div className="pl-8">
-              <label htmlfor="role" className="text-white">
-                Ver y comprar diseños únicos
-              </label>
-              <input
-                type="checkbox"
-                id="user"
-                name="role"
-                value="user"
-                onChange={handleChange}
-              />
+          <div className="flex flex-col">
+            <div className="text-center text-white">¿Qué te gustaría hacer</div>
+            <br />
+            <div className="py-5 px-4 flex">
+              <div role="group" aria-labelledby="checkbox-group">
+                <label htmlFor="role" className="text-white">
+                  Promover y diseñar productos
+                </label>
+                <Field
+                  className="px-4"
+                  type="checkbox"
+                  name="role"
+                  value="seller"
+                />
+                <label htmlFor="role pl-8" className="text-white">
+                  Ver y comprar diseños únicos
+                </label>
+                <Field
+                  className="px-4"
+                  type="checkbox"
+                  name="role"
+                  value="user"
+                />
+                 <ErrorMessage  name="role" /> 
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className=" py-5">
-          <h3 className="text-center text-white">¿Cuál es tu género?</h3>
-          <div className="flex justify-around">
-            <div>
-              <label htmlfor="sex" className="text-white">
-                male
-              </label>
-              <input
-                type="checkbox"
-                id="sex"
-                name="sex"
-                value="male"
-                onChange={handleChange}
-              />
+          <div className="flex flex-col py-8">
+            <div className="text-center text-white">¿Cuál es tu género</div>
+            <br />
+           
+              <div role="group" className="px-4 flex"aria-labelledby="checkbox-group">
+                <label htmlFor="sex" className="text-white">
+                  Hombre
+                </label>
+                <Field
+                  className="px-4"
+                  type="checkbox"
+                  name="sex"
+                  value="male"
+                />
+                <label htmlFor="sex" className="text-white">
+                  Mujer
+                </label>
+                <Field
+                  className="px-4"
+                  type="checkbox"
+                  name="sex"
+                  value="female"
+                />
+              </div>
+              <br />
+             
             </div>
-            <div>
-              {" "}
-              <label htmlfor="role" className="text-white">
-                female
-              </label>
-              <input
-                type="checkbox"
-                id="sex"
-                name="sex"
-                value="female"
-                onChange={handleChange}
-              />
-            </div>
+            <ErrorMessage  name="sex" /> 
+          
+
+          <div>
+            <label className=" text-white py-8">
+              <Field type="checkbox" name=" termsUsers" />
+              He leído y estoy de acuerdo con las políticas de privacidad.
+              (*requerido)
+            </label>
+            <br />
+            <ErrorMessage name="acceptedTerms" />
+            <br />
+
+            <label className=" text-white pb-12">
+              <Field type="checkbox" name="Subscribe" />
+              Suscribirse para recibir información especial sobre Valen vía
+              email.
+            </label>
           </div>
-        </div>
-        <div className="formulario__checkbox py-4">
-          <input
-            className="accent-[#F4F0BB]"
-            type="checkbox"
-            value="first_checkbox"
-          />
-          <span className=" text-white py-8">
-            He leído y estoy de acuerdo con las políticas de privacidad.
-            (*requerido)
-          </span>
-          <br />
-          <input
-            className="text-[#F4F0BB]"
-            type="checkbox"
-            value="first_checkbox"
-          />
-          <span className=" text-white py-8">
-            Suscribirse para recibir información especial sobre Valen vía email.
-          </span>
-        </div>
-
-        <button
-          className="  col-start-2 col-end-4 w-full shadow-2xl bg-[#F4F0BB] text-[#1B5B45]  rounded-md py-2"
-          type="submit"
-        >
-          Crear cuenta
-        </button>
-      </form>
-    </div>
+          <button
+            className="  col-start-2 col-end-4 w-full shadow-2xl bg-[#F4F0BB] text-[#1B5B45]  rounded-md py-2 mt-10"
+            type="submit"
+          >
+            Crear cuenta
+          </button>
+        </Form>
+      </div>
+    </Formik>
   );
 }
-
-export default signup;
